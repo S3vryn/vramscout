6cf7c8aa4f373ec70fc784c0a52f4bc4df669429
Junchi Ma <junchima.cn@gmail.com>
2026-09-09 10:29:21 +0800

    Bump VRAMScout to v0.7.0

--- src/vramscout/__init__.py
+++ src/vramscout/__init__.py
@@ -1,7 +1,8 @@
-"""VRAMScout: architecture-aware LLM deployment memory planning."""
+"""VRAMScout: architecture- and engine-aware LLM deployment memory planning."""
 
-from .modern_v06 import inspect_modern_model, plan_modern
+from .modern_v06 import inspect_modern_model
+from .modern_v07 import plan_modern
 from .planner import plan_inference
 
 __all__ = ["plan_modern", "inspect_modern_model", "plan_inference"]
-__version__ = "0.6.0"
+__version__ = "0.7.0"
